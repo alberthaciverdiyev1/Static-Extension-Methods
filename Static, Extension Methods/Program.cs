944cae9605e923a58284f165cd51ac8738d937b6
Alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-04-17 20:41:27 +0400

    20:41

---
+++ Static, Extension Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using Static__Extension_Methods.Utilities;
+
+namespace Static__Extension_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1 = 3;
+            int Number2 = 4;
+            int Number3 = 5;
+            int Number4 = 6;
+            Console.WriteLine($"Daxil etdiyiniz {Number1} reqeminin tek olmasi {Number1.IsOdd()}-dur");
+            Console.WriteLine($"Daxil etdiyiniz {Number2} reqeminin tek olmasi {Number2.IsOdd()}-dur");
+            Console.WriteLine($"Daxil etdiyiniz {Number3} reqeminin cut olmasi {Number3.IsEven()}-dur");
+            Console.WriteLine($"Daxil etdiyiniz {Number4} reqeminin cut olmasi {Number4.IsEven()}-dur");
+
+
+            string text1 = "albert";
+            string text2 = "albert159";
+            Console.WriteLine($"Daxil etdiyiniz {text1} sozunde reqem olmasi {text1.HasDigit()}-dur");
+            Console.WriteLine($"Daxil etdiyiniz {text2} sozunde reqem olmasi {text2.HasDigit()}-dur");
+
+
+            string password1 = "albertDeneme123";
+            string password2 = "AlbertDeneme";
+
+            Console.WriteLine($"Daxil etdiyiniz {password1} sifresinin duzgunluyu {password1.CheckPassword()}-dur");
+            Console.WriteLine($"Daxil etdiyiniz {password2} sifresinin duzgunluyu {password2.CheckPassword()}-dur");
+
+
+            string text3 = "hal-hazirda saat 20:31-dir";
+
+            Console.WriteLine(text3.Capitalize());
+
+
+        }
+    }
+}
---
+++ Static, Extension Methods/Utilities/Helper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static__Extension_Methods.Utilities
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+        public static bool HasDigit(this string text)
+        {
+            foreach (char item in text)
+            {
+                if (char.IsDigit(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckPassword(this string text)
+        {
+            int HasDigit = 0;
+            int HasBigLetter = 0;
+
+            foreach (char item in text)
+            {
+                if (char.IsLetter(item))
+                {
+                    HasDigit++;
+                }
+                else if (char.IsDigit(item))
+                {
+                    HasBigLetter++;
+                }
+
+            }
+            if (HasDigit > 0 && HasBigLetter > 0 && text.Length >= 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static string Capitalize(this string text)
+        {
+            return char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+    }
+}
